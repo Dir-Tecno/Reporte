b2d10d8ba0db2bde68c0029af327d75d5de59b1f
LucaSalvador <lucaalaporta@gmail.com>
2024-11-29 13:05:13 +0000

    Cambio puestos

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from moduls.carga import load_data_from_bucket
 from moduls.inscripciones import show_inscriptions
 from moduls.empresas import show_companies
-#from moduls.respuestas import show_responses
+from moduls.puestos import show_puestos
 
 
 # Configuración de las credenciales
